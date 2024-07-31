c0c6c4fe1113c5948c10bbb27ba31104c019e399
Sumit Pandey <heerjnv@gmail.com>
2024-07-31 15:59:57 +0530

    Done

--- src/component/AddMovieForm.js
+++ src/component/AddMovieForm.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddMovieForm = () => {
+  const [formState, setFormState] = useState({
+    title: "",
+    opening_text: "",
+    release_date: "",
+  });
+
+  function onTitleChangeHandler(e) {
+    e.preventDefault();
+    setFormState({
+      ...formState,
+      title: e.target.value,
+    });
+  }
+  function onOpeningTextChangeHandler(e) {
+    e.preventDefault();
+    setFormState({
+      ...formState,
+      opening_text: e.target.value,
+    });
+  }
+  function onReleaseDateChangeHandler(e) {
+    e.preventDefault();
+    setFormState({
+      ...formState,
+      release_date: e.target.value,
+    });
+  }
+
+  function formSubmitHandler(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
   return (
-    <div className="flex flex-col items-start">
+    <form onSubmit={formSubmitHandler} className="flex flex-col items-start">
       <label className="font-bold mt-3">Title</label>
       <input
         className="w-full border rounded-xl"
         type="text"
         name="title"
         id="title"
+        onChange={onTitleChangeHandler}
       />
 
       <label className="font-bold mt-3">Opening Text</label>
@@ -17,6 +51,7 @@ const AddMovieForm = () => {
         type="text"
         name="opening_text"
         id="opening_text"
+        onChange={onOpeningTextChangeHandler}
       />
 
       <label className="font-bold mt-3">Release Date</label>
@@ -25,12 +60,16 @@ const AddMovieForm = () => {
         type="text"
         name="release_date"
         id="release_date"
+        onChange={onReleaseDateChangeHandler}
       />
 
-      <button className="self-center bg-blue-600 text-white px-5 py-2 my-3 rounded-2xl">
+      <button
+        type="submit"
+        className="self-center bg-blue-600 text-white px-5 py-2 my-3 rounded-2xl"
+      >
         Add Movie
       </button>
-    </div>
+    </form>
   );
 };
 
